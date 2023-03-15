ff2ecad41bee31cfa2ab7e5d3c18caf5421bf1f6
Roselle Mata <roselle.tmata@gmail.com>
2023-03-15 11:21:27 -0500

    Author quiz render

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './bootstrap.min.css';
+import React, { Component } from 'react';
+
+class AuthorQuiz extends Component {
+  render() {
+  return (
+      <div>Author Quiz</div>
+      );
+  }
+}
+
+
+
+export default AuthorQuiz;
